fb1b5f72d249f09b5619ee4ce044b6816bc64164
unknown <kenji.kitaura@.HOCC3.jp>
2008-10-13 16:28:42 +0900

    book new css add

--- app/models/amazon_book.rb
+++ app/models/amazon_book.rb
@@ -1,20 +1,29 @@
 class AmazonBook
-  attr_accessor :asin, :title, :image
+  attr_accessor :isbn, :title, :image, :auther
   
   def AmazonBook.serch_by_title title
     res = Amazon::Ecs.item_search(title, :response_group => 'Large')
     amazon_books = res.items
     amazon_books.collect{|item|
-      amazon_asin = item.get('asin')
+      amazon_isbn = item.get('itemattributes/isbn')
       amazon_title = item.get('itemattributes/title')
       amazon_image = item.get('mediumimage/url')
-      AmazonBook.new(amazon_asin,amazon_title,amazon_image)
+      amazon_image ||= '/images/noimage.gif'
+      amazon_auther = item.get('itemattributes/author')
+      
+      AmazonBook.new(amazon_isbn,amazon_title,amazon_image,amazon_auther)
     }
   end
   
-    def initialize(asin, title = nil, image = nil)
-    @asin = asin
+  def AmazonBook.serch hash
+    
+    
+  end
+  
+  def initialize(isbn, title = nil, image = nil, auther = nil)
+    @isbn = isbn
     @title = title
     @image = image
+    @auther = auther
   end
 end
